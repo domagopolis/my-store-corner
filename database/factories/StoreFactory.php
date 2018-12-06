8f6b6b24405d44cc89638cad1312621ed7824370
Josh <josh.domagala@outlook.com>
2018-12-07 00:48:03 +1100

    Complete Stores seeder

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -4,6 +4,19 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Store::class, function (Faker $faker) {
     return [
-        //
+        'postcode_id' => App\Postcode::all()->random()->id,
+        'username' => $faker->userName,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        'name' => $faker->company,
+        'slug' => $faker->slug,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'mobile' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'business_registration_number' => $faker->randomNumber,
+        'website' => 'https://www.'.$faker->domainName,
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude,
+        'payed_member' => 1,
     ];
 });
--- database/seeds/StoresTableSeeder.php
+++ database/seeds/StoresTableSeeder.php
@@ -11,19 +11,8 @@ class StoresTableSeeder extends Seeder
      */
 	public function run()
 	{
-		for($i=0; $i<1000; $i++){
-			DB::table('stores')->insert([
-				'country_id' => 1,
-				'state_id' => 1,
-				'postcode_id' => 1,
-				'slug' => str_random(10),
-				'username' => str_random(10),
-				'email' => str_random(10).'@gmail.com',
-				'password' => bcrypt('secret'),
-				'name' => str_random(10),
-				'business_registration_number' => str_random(10),
-				'address' => str_random(10),
-			]);
-		}
+		factory(App\Store::class, 100)->create()->each(function($wps) {
+            ;
+        });
 	}
 }
