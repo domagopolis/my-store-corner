65fa799f58ff6ac5fdfddabb0566f774395cd1e7
Josh <josh.domagala@outlook.com>
2019-02-17 18:09:19 +1100

    Update contact us label

--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -7,17 +7,17 @@
 <form action="{{ url('contact') }}" method="POST">
 	{{ csrf_field() }}
 	<div class="form-group">
-		<label name="email">Email:</label>
+		<label name="email" for="email">Email:</label>
 		<input id="email" name="email" class="form-control">
 	</div>
 
 	<div class="form-group">
-		<label name="subject">Subject:</label>
+		<label name="subject" for="subject">Subject:</label>
 		<input id="subject" name="subject" class="form-control">
 	</div>
 
 	<div class="form-group">
-		<label name="message">Message:</label>
+		<label name="message" for="message">Message:</label>
 		<textarea id="message" name="message"></textarea>
 	</div>
 	<button type="submit" name="Send Message">Send Message</button>
