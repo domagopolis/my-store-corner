a002b57a92642c4dfce818700a3d355d3fcd3dcb
Josh <josh.domagala@outlook.com>
2019-02-22 13:33:40 +1100

    store page migration and seeder

---
+++ database/seeds/StorePagesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StorePagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
